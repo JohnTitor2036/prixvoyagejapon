c2f8fd743ff0e78c65db9b179f5e724e6fd4bb29
JohnTitor2036 <nicolas.meunier@hotmail.ca>
2020-02-15 12:02:38 -0500

    Kansai et Kansai wide

--- scripts/constantes.js
+++ scripts/constantes.js
@@ -1,31 +1,38 @@
-//https://www.japan-rail-pass.fr/jr-pass
+//Prix en CAD
 
-//JR Pass National
-const JRP_NAT_7_ORD_CHILD = 178;
-const JRP_NAT_14_ORD_CHILD = 284;
-const JRP_NAT_21_ORD_CHILD = 363;
+//JR Pass National : https://www.japan-rail-pass.fr/jr-pass
+const JRP_NAT_7_ORD_CHILD = 180;
+const JRP_NAT_14_ORD_CHILD = 287;
+const JRP_NAT_21_ORD_CHILD = 367;
 
-const JRP_NAT_7_ORD_ADULT = 356;
-const JRP_NAT_14_ORD_ADULT = 567;
-const JRP_NAT_21_ORD_ADULT = 725;
+const JRP_NAT_7_ORD_ADULT = 359;
+const JRP_NAT_14_ORD_ADULT = 573;
+const JRP_NAT_21_ORD_ADULT = 733;
 
-const JRP_NAT_7_GREEN_CHILD = 238;
-const JRP_NAT_14_GREEN_CHILD = 385;
-const JRP_NAT_21_GREEN_CHILD = 500;
+const JRP_NAT_7_GREEN_CHILD = 240;
+const JRP_NAT_14_GREEN_CHILD = 389;
+const JRP_NAT_21_GREEN_CHILD = 506;
 
-const JRP_NAT_7_GREEN_ADULT = 475;
-const JRP_NAT_14_GREEN_ADULT = 769;
-const JRP_NAT_21_GREEN_ADULT = 999;
+const JRP_NAT_7_GREEN_ADULT = 480;
+const JRP_NAT_14_GREEN_ADULT = 778;
+const JRP_NAT_21_GREEN_ADULT = 1012;
 
-//JR Pass Régionaux
+//JR Pass Régionaux : https://www.japan-rail-pass.fr/pass-regional
 
 //Kansai
-const JRP_KANSAI_1_ADULT = 475;
-const JRP_KANSAI_2_ADULT = 475;
-const JRP_KANSAI_3_ADULT = 475;
-const JRP_KANSAI_4_ADULT = 475;
+const JRP_KANSAI_1_CHILD = 14;
+const JRP_KANSAI_2_CHILD = 28;
+const JRP_KANSAI_3_CHILD = 34;
+const JRP_KANSAI_4_CHILD = 40;
 
-const JRP_KANSAI_1_CHILD = 475;
-const JRP_KANSAI_2_CHILD = 475;
-const JRP_KANSAI_3_CHILD = 475;
-const JRP_KANSAI_4_CHILD = 475;
+const JRP_KANSAI_1_ADULT = 28;
+const JRP_KANSAI_2_ADULT = 56;
+const JRP_KANSAI_3_ADULT = 68;
+const JRP_KANSAI_4_ADULT = 80;
+
+//Kansai wide
+const JRP_KANSAI_WIDE_5_CHILD = 56;
+
+const JRP_KANSAI_WIDE_5_ADULT = 111;
+
+//Hokkaido
